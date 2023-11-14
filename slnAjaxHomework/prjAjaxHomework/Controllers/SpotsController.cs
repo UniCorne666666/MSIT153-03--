186ff20f81dd13a00697d0086a1985c42ff7c95b
子倫 黃 <alan90306@gmail.com>
2023-11-14 19:49:00 +0800

    test1

---
+++ slnAjaxHomework/prjAjaxHomework/Controllers/SpotsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace prjAjaxHomework1.Controllers
+{
+    public class SpotsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
